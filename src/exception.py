e4a5ad2b878803fb1feaae91bebdf0df05175b02
rajasekhar <rajasekharreddykamireddy36@gmail.com>
2024-09-19 19:35:55 +0530

    created all pipelines and front end

--- src/exception.py
+++ src/exception.py
@@ -8,8 +8,7 @@ def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error_message = f"Error occurred in script: {file_name} at line {line_number} with error message: {error}".format(
-        file_name,exc_tb.tb_lineno,str(error))
+    error_message = f"Error occurred in script: {file_name} at line {line_number} with error message: {error}".format(file_name,exc_tb.tb_lineno,str(error))
 
     logging.error(error_message)
     return error_message
